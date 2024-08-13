d5bd732a32f196df4434b61b1e2d16780db34bb9
Johnny Tsoi <kiucheongchoi@gmail.com>
2024-08-12 21:08:57 -0700

    Solved 104 again

--- LeetCode.playground/Pages/104. Maximum Depth of Binary Tree.xcplaygroundpage/Contents.swift
+++ LeetCode.playground/Pages/104. Maximum Depth of Binary Tree.xcplaygroundpage/Contents.swift
@@ -1,7 +1,7 @@
 /*:
  ## Binary Tree, Recursion
- ### Time: O(n)
- ### Space: O(㏒n)
+ ### Time: O(N)
+ ### Space: O(N)
  ![submission](1.png)
  */
 class Solution {
@@ -17,7 +17,49 @@ let s = Solution()
 s.maxDepth(TreeNode(3, TreeNode(9), TreeNode(20, TreeNode(15), TreeNode(7))))
 s.maxDepth(TreeNode(1, nil, TreeNode(2)))
 
+/*:
+ ## Binary Tree, Breadth First Search
+ ### Time: O(N)
+ ### Space: O(N)
+ ![submission](2.png)
+ */
 class Solution2 {
+    func maxDepth(_ root: TreeNode?) -> Int {
+         guard let root else { return 0 }
+
+         var queue = [root]
+         var depth = 0
+         
+         while !queue.isEmpty {
+            let size = queue.count
+            var curLevel = [TreeNode]()
+
+            for i in 0..<size {
+                let node = queue[i]
+
+                if let left = node.left {
+                    curLevel.append(left)
+                }
+                if let right = node.right {
+                    curLevel.append(right)
+                }
+            }
+            depth += 1
+            queue = curLevel
+         }
+
+         return depth
+    }
+}
+
+/*:
+ ## Binary Tree, Iteration
+ ### Time: O(N)
+ ### Space: O(N)
+ ![submission](3.png)
+ */
+
+class Solution3 {
     func maxDepth(_ root: TreeNode?) -> Int {
         guard let root = root else { return 0 }
         var stack = [(Int, TreeNode?)]()
@@ -39,10 +81,10 @@ class Solution2 {
     }
 }
 
-let s2 = Solution2()
+let s3 = Solution3()
 
-s2.maxDepth(TreeNode(3, TreeNode(9), TreeNode(20, TreeNode(15), TreeNode(7))))
-s2.maxDepth(TreeNode(1, nil, TreeNode(2)))
+s3.maxDepth(TreeNode(3, TreeNode(9), TreeNode(20, TreeNode(15), TreeNode(7))))
+s3.maxDepth(TreeNode(1, nil, TreeNode(2)))
 
 public class TreeNode {
     public var val: Int
