d696b8e0749713523a0d7bf530d1ed9b2391bdf7
Johnny Tsoi <kiucheongchoi@gmail.com>
2024-08-17 18:29:12 -0700

    Solved 263 again

--- LeetCode.playground/Pages/263. Ugly Number.xcplaygroundpage/Contents.swift
+++ LeetCode.playground/Pages/263. Ugly Number.xcplaygroundpage/Contents.swift
@@ -6,18 +6,37 @@
 class Solution {
     func isUgly(_ n: Int) -> Bool {
         var n = n
-        
-        for prime in [2,3,5] {
+
+        for prime in [2, 3, 5] {
             while n > 0, n % prime == 0 {
                 n /= prime
             }
         }
-        
+
         return n == 1
     }
 }
 
 let s = Solution().isUgly
+
+/*:
+ ### Time: O(log(N))
+ ### Space: O(log(N))
+ ![submission](2.png)
+ */
+class Solution2 {
+    func isUgly(_ n: Int) -> Bool {
+        if n == 1 {
+            return true
+        }
+        for p in [2, 3, 5] where n > 0 {
+            if n % p == 0 {
+                return isUgly(n / p)
+            }
+        }
+        return false
+    }
+}
 
 s(6)
 s(1)
