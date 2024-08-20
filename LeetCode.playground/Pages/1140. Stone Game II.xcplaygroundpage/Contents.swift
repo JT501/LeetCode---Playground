f636b76c09af0fa587b1a7a279349945de5350ee
Johnny Tsoi <kiucheongchoi@gmail.com>
2024-08-20 00:24:30 -0700

    Solved 1140

--- LeetCode.playground/Pages/1140. Stone Game II.xcplaygroundpage/Contents.swift
+++ LeetCode.playground/Pages/1140. Stone Game II.xcplaygroundpage/Contents.swift
@@ -1,7 +1,37 @@
-//: [Previous](@previous)
+/*:
+ ## Dynamic Programming
+ ### Time: O(N³)
+ ### Space: O(N²)
+ ![submission](1.png)
+ */
 
-import Foundation
+class Solution {
+    func stoneGameII(_ piles: [Int]) -> Int {
+        var m = 1
+        var len = piles.count
+        var dp = Array(repeating: Array(repeating: 0, count: len + 1), count: len)
+        var prefixSum = Array(repeating: 0, count: len + 1)
 
-var greeting = "Hello, playground"
+        for i in 1..<len+1 {
+            prefixSum[i] = piles[i-1] + prefixSum[i-1]
+        }
 
-//: [Next](@next)
+        func dfs(start: Int, m: Int) -> Int {
+            if dp[start][m] > 0 {
+                return dp[start][m]
+            }
+            if 2 * m >= len - start {
+                return prefixSum[len] - prefixSum[start]
+            }
+
+            var maxStones = 0
+            for x in 1...2*m {
+                maxStones = max(maxStones, prefixSum[len] - prefixSum[start] - dfs(start: start + x, m: max(m, x)))
+            }
+            dp[start][m] = maxStones
+            return maxStones
+        }
+
+        return dfs(start: 0, m: 1)
+    }
+}
