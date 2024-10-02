267082abc827eb37e1534664a050c32689e87dfb
Johnny Tsoi <kiucheongchoi@gmail.com>
2024-10-01 17:29:51 -0700

    Solved 1331

---
+++ LeetCode.playground/Pages/1331. Rank Transform of an Array.xcplaygroundpage/Contents.swift
@@ -0,0 +1,20 @@
+/*:
+ ### Time: O(1)
+ ### Space: O(n)
+ ![submission](1.png)
+ */
+
+class Solution {
+    func arrayRankTransform(_ arr: [Int]) -> [Int] {
+        let sorted = arr.sorted()
+        var hashmap = [Int:Int]()
+    
+        for num in sorted {
+            if hashmap[num] == nil {
+                hashmap[num] = hashmap.count + 1
+            }
+        }
+
+        return arr.map { hashmap[$0]! }
+    }
+}
