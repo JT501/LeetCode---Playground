a0b60d913c93bdeeef787b86138f64448f519253
Johnny Tsoi <kiucheongchoi@gmail.com>
2025-01-05 21:18:03 -0800

    Solved 1769

--- LeetCode.playground/Pages/1769. Minimum Number of Operations to Move All Balls to Each Box.xcplaygroundpage/Contents.swift
+++ LeetCode.playground/Pages/1769. Minimum Number of Operations to Move All Balls to Each Box.xcplaygroundpage/Contents.swift
@@ -1,7 +1,35 @@
-//: [Previous](@previous)
+/*:
+ ## 
+ ### Time: O(N²)
+ ### Space: O(N2)
+ ![submission](1.png)
+ */
 
-import Foundation
+class Solution {
+    func minOperations(_ boxes: String) -> [Int] {
+        let len = boxes.count
+        var idice = [Int]()
+        var ans = [Int]()
 
-var greeting = "Hello, playground"
+        for (i, box) in boxes.enumerated() {
+            if box == "1" {
+                idice.append(i)
+            }
+        }
 
-//: [Next](@next)
+        for j in 0..<len {
+            var op = 0
+            for idx in idice {
+                if idx == j { continue}
+                if idx > j {
+                    op += idx - j
+                } else {
+                    op += j - idx
+                }
+            }
+            ans.append(op)
+        }
+
+        return ans
+    }
+}
