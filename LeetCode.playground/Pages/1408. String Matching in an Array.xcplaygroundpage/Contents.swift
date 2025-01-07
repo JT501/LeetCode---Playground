eef0d241e85059c44b233582e215bee7cd388a60
Johnny Tsoi <kiucheongchoi@gmail.com>
2025-01-06 23:24:00 -0800

    Solved 1408

---
+++ LeetCode.playground/Pages/1408. String Matching in an Array.xcplaygroundpage/Contents.swift
@@ -0,0 +1,25 @@
+/*:
+ ##
+ ### Time: O(N²)
+ ### Space: O(N)
+ ![submission](1.png)
+ */
+
+class Solution {
+    func stringMatching(_ words: [String]) -> [String] {
+        let words = words.sorted(by: { $0.count < $1.count })
+        var ans = [String]()
+
+        for word in words {
+            for w in words.reversed() {
+                guard word.count < w.count else { break }
+                if w.contains(word) {
+                    ans.append(word)
+                    break
+                }
+            }
+        }
+
+        return ans
+    }
+}
