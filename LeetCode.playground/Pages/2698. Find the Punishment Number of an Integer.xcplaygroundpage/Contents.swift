af2140c07dfe0843fd0d2b641f7cdd43b78213fb
Johnny Tsoi <kiucheongchoi@gmail.com>
2025-02-14 22:04:04 -0800

    Solved 2698

---
+++ LeetCode.playground/Pages/2698. Find the Punishment Number of an Integer.xcplaygroundpage/Contents.swift
@@ -0,0 +1,33 @@
+/*:
+ ## Recursive
+ ### Time: O(n*2^log10​(n))
+ ### Space: O(log10​(n))
+ ![submission](1.png)
+ */
+class Solution {
+    func punishmentNumber(_ n: Int) -> Int {
+        var sum = 0
+
+        for i in 1...n {
+            if isSummable(num: i * i, sum: i) {
+                sum += i * i
+            }
+        }
+
+        return sum
+        
+        func isSummable(num: Int, sum: Int) -> Bool {
+            if num == sum { return true }
+
+            var div = 10
+
+            while div < num {
+                let (left, right) = (num / div, num % div)
+                if isSummable(num: left, sum: sum - right) { return true }
+                div *= 10
+            }
+
+            return false
+        }
+    }
+}
